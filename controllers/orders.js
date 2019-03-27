71ce5801b66c2b62848b33f938d10df72687615b
opentech4u@gmail.com <opentech4u@gmail.com>
2019-03-27 19:19:09 +0530

    order complete

--- controllers/orders.js
+++ controllers/orders.js
@@ -4,9 +4,99 @@ const router = require('express').Router(),
 
 router.get('/', (req, res) => {
 
-    Orders.getOrders((data) => {
+    Orders.getOrders(req.data.user.user_name, (data) => {
         res.send(data);
     });
 
 });
+
+router.get('/dependencies', (req, res) => {
+
+    var dataObj = {};
+
+    Orders.getAll('md_district', (data) => {
+        dataObj.dist = data;
+    });
+
+    Orders.getAll('md_project_type', (data) => {
+        dataObj.projectType = data;
+        res.send(dataObj);
+    });
+
+});
+
+router.get('/getorder/:id', (req, res) => {
+
+    data = {
+        id: req.params.id,
+        user: req.data.user.user_name
+    }
+    Orders.getOrder(data, (data) => {
+        res.send(data);
+    });
+
+});
+
+router.post('/details', (req, res) => {
+
+    let data = {
+        project_name: req.body.prjectName ,
+        project_type: req.body.projectType ,
+        contact_person: req.body.socPerson ,
+        contact_no: req.body.contactNo ,
+        designation: req.body.designation ,
+        dist: req.body.dist ,
+        block: req.body.block ,
+        order_dt: req.body.orderDt ,
+        order_dtls: req.body.orderDetails ,
+        order_value: req.body.orderValue ,
+        tax: req.body.tax ,
+        payment_terms: req.body.paymentTerms ,
+        monthly_rental: req.body.monthlyRental ,
+        payment_status: req.body.paymentStatus,
+        sales_person:req.body.salePerson , 
+        installed_by: req.body.installedBy ,
+        installation_dt: req.body.installationDate ,
+        sss_remarks: req.body.sssRemarks ,
+        cust_remarks: req.body.customerRemarks ,
+        created_by: req.data.user.user_name,
+        created_dt: common.formatDate(new Date()),
+    }
+    Orders.addDtls(data, () => {
+        res.send({msg: 'success'});
+    });
+
+});
+
+router.put('/editdetails/:id', (req, res) => {
+
+    let data = {
+        project_name: req.body.prjectName ,
+        project_type: req.body.projectType ,
+        contact_person: req.body.socPerson ,
+        contact_no: req.body.contactNo ,
+        designation: req.body.designation ,
+        dist: req.body.dist ,
+        block: req.body.block ,
+        order_dt: req.body.orderDt ,
+        order_dtls: req.body.orderDetails ,
+        order_value: req.body.orderValue ,
+        tax: req.body.tax ,
+        payment_terms: req.body.paymentTerms ,
+        monthly_rental: req.body.monthlyRental ,
+        payment_status: req.body.paymentStatus,
+        sales_person:req.body.salePerson , 
+        installed_by: req.body.installedBy ,
+        installation_dt: req.body.installationDate ,
+        sss_remarks: req.body.sssRemarks ,
+        cust_remarks: req.body.customerRemarks ,
+        created_by: req.data.user.user_name,
+        created_dt: common.formatDate(new Date()),
+    }
+    Orders.editDtls(data, req.params.id ,() => {
+        res.send({msg: 'success'});
+    });
+    
+});
+
 module.exports = router;
--- models/orders.js
+++ models/orders.js
@@ -3,7 +3,7 @@ const db = require('../config/database'),
 
 const orders = {};
 
-orders.getOrders = (cb) => {
+orders.getOrders = (user, cb) => {
 
     let sql = `SELECT t.id,
                       t.project_name,
@@ -14,7 +14,38 @@ orders.getOrders = (cb) => {
                       t.*
                FROM td_project_details t, md_district dst, md_project_type pt
                WHERE t.project_type = pt.type_cd
-               AND t.dist = dst.district_code`;
+               AND t.dist = dst.district_code
+               AND t.created_by = ?`;
+
+    db.query(sql, [user], (err, result) => {
+
+        if (err) throw err;
+
+        cb(result);
+
+    });
+
+}
+
+orders.getOrder = (data, cb) => {
+
+    let sql = `SELECT * FROM td_project_details
+               WHERE id = ? 
+               AND created_by = ?`;
+
+    db.query(sql, [data.id, data.user], (err, result) => {
+
+        if (err) throw err;
+
+        cb(result);
+
+    });
+
+}
+
+orders.getAll = (tableName, cb) => {
+
+    let sql = `SELECT * FROM ${tableName}`;
 
     db.query(sql, (err, result) => {
 
@@ -24,6 +55,117 @@ orders.getOrders = (cb) => {
 
     });
 
+}
+
+orders.addDtls = (data, cb) => {
+
+    let sql = `INSERT INTO td_project_details ( project_name,
+                                                project_type,
+                                                contact_person,
+                                                contact_no,
+                                                designation,
+                                                dist,
+                                                block,
+                                                order_dt,
+                                                order_dtls,
+                                                order_value,
+                                                tax,
+                                                payment_terms,
+                                                monthly_rental,
+                                                payment_status,
+                                                sales_person,
+                                                installed_by,
+                                                installation_dt,
+                                                sss_remarks,
+                                                cust_remarks,
+                                                created_by,
+                                                created_dt
+                                            )
+                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+
+    db.query(sql, [ data.project_name,
+                    data.project_type,
+                    data.contact_person,
+                    data.contact_no,
+                    data.designation,
+                    data.dist,
+                    data.block,
+                    data.order_dt,
+                    data.order_dtls,
+                    data.order_value,
+                    data.tax,
+                    data.payment_terms,
+                    data.monthly_rental,
+                    data.payment_status,
+                    data.sales_person,
+                    data.installed_by,
+                    data.installation_dt,
+                    data.sss_remarks,
+                    data.cust_remarks,
+                    data.created_by,
+                    data.created_dt
+    ], (err, result) => {
+
+        if (err) throw err;
+
+        cb(result);
+
+    });
+}
+
+orders.editDtls = (data, id, cb) => {
+
+let sql = `UPDATE td_project_details SET project_name = ?,
+                                         project_type = ?,
+                                         contact_person = ?,
+                                         contact_no = ?,
+                                         designation = ?,
+                                         dist = ?,
+                                         block = ?,
+                                         order_dt = ?,
+                                         order_dtls = ?,
+                                         order_value = ?,
+                                         tax = ?,
+                                         payment_terms = ?,
+                                         monthly_rental = ?,
+                                         payment_status = ?,
+                                         sales_person = ?,
+                                         installed_by = ?,
+                                         installation_dt = ?,
+                                         sss_remarks = ?,
+                                         cust_remarks = ?,
+                                         created_by = ?,
+                                         created_dt = ?
+                WHERE id = ${id}`;
+
+    db.query(sql, [ data.project_name,
+                    data.project_type,
+                    data.contact_person,
+                    data.contact_no,
+                    data.designation,
+                    data.dist,
+                    data.block,
+                    data.order_dt,
+                    data.order_dtls,
+                    data.order_value,
+                    data.tax,
+                    data.payment_terms,
+                    data.monthly_rental,
+                    data.payment_status,
+                    data.sales_person,
+                    data.installed_by,
+                    data.installation_dt,
+                    data.sss_remarks,
+                    data.cust_remarks,
+                    data.created_by,
+                    data.created_dt
+    ], (err, result) => {
+
+        if (err) throw err;
+
+        cb(result);
+
+    });
 }
 
 module.exports = orders;
